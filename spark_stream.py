316d4cac72ba47b47c893311b384b3fbe73afee9
Rishit Saharan <rishitsaharanbiz@gmail.com>
2024-10-12 19:17:22 +0530

    updated docker links

--- spark_stream.py
+++ spark_stream.py
@@ -87,7 +87,7 @@ def connect_to_kafka(spark_conn):
     try:
         spark_df = spark_conn.readStream \
             .format('kafka') \
-            .option('kafka.bootstrap.servers', 'localhost:9092') \
+            .option('kafka.bootstrap.servers', 'broker:29092') \
             .option('subscribe', 'users_created') \
             .option('startingOffsets', 'earliest') \
             .load()
